331871b710625d65e660e9c1c437b914f9dd3d82
TheScarastic <warabhishek@gmail.com>
2018-12-28 15:13:24 -0500

    sm6150-common: parts: Add DiracSound backend
    Special thanks to vasishath for his base work on mi3
    
    @neobuddy89: Clean up.
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I1e1ae20a5e1b0f6c16677e1f43c4dba18f632c7d

--- sm6150.mk
+++ sm6150.mk
@@ -173,6 +173,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-parts.xml
+
 # Display/Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
