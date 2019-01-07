773b85e75878913604922125b3595d19f1e11ced
Michael Bestas <mkbestas@lineageos.org>
2019-01-08 01:51:52 +0200

    sm6150-common: rootdir: Import init.qcom.early_boot.sh
    * We used to have an init extension, which breaks treble
      and since P it requires custom SELinux rules to allow
      init reading /proc/sys/kernel/boot_reason
    * Remove the init extension and set the property like
      QCOM does through init.qcom.early_boot.sh
    * Update the property with vendor prefix while we are at it
    
    Change-Id: I238f754a17c9a548c6c90503bfa4b21763d1b4c4
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -194,6 +194,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.zram:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.zram
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/bin/init.qcom.early_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.early_boot.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.post_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.post_boot.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.sh
 
