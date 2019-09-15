fd29b5c333995dd3fb3e28f3648ad21046b9f066
PIPIPIG233666 <pig.priv@gmail.com>
2019-09-14 21:46:19 -0400

    sm6150-common: audio: Rename audio_tuning_mixer_tavil.txt to audio_tuning_mixer.txt
     * Silence the following:
       ACDB-MCS: acdb_mcs_init: /vendor/etc/audio_tuning_mixer.txt not present, using /vendor/etc/audio_tuning_mixer.txt config file
       MCS-RT-CTL: Can't open the configuration file /vendor/etc/audio_tuning_mixer.txt.
    
    Change-Id: I959588613a61c57b417e93a38757281fb178f586

--- sm6150.mk
+++ sm6150.mk
@@ -56,7 +56,11 @@ PRODUCT_PACKAGES += \
     libaudio-resampler
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+    $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    $(LOCAL_PATH)/configs/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/configs/audio/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
+    $(LOCAL_PATH)/configs/audio/bluetooth_qti_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_qti_audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
