40f7de8f2f556ec8cfc068a90133a925e65452e3
Mikhail Naganov <mnaganov@google.com>
2019-11-22 17:34:45 -0800

    sm6150-common: Switch to Audio HAL V6
    Switch sunfish to Audio HAL V6
    
    Test: VtsHalAudioV6_0TargetTest VtsHalAudioEffectV6_0TargetTest
    Bug: 155591007
    Change-Id: If40ae28c224160728634736ee95590a779f905c4
    (cherry picked from commit f11963392c9afd79704822a5812ef4412f3810db)

--- sm6150.mk
+++ sm6150.mk
@@ -97,12 +97,16 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@6.0 \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@4.0 \
     android.hardware.audio@4.0-impl \
     android.hardware.audio@5.0 \
     android.hardware.audio@5.0-impl \
+    android.hardware.audio@6.0 \
+    android.hardware.audio@6.0-impl \
     android.hardware.soundtrigger@2.2-impl:32 \
     audio.a2dp.default \
     audio.primary.sm6150:32 \
