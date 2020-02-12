eda6e66a047e17bb9f65a6e0f78c144a30da5258
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    sm6150-common: Enable freeform window management
    Change-Id: I2de7aab9d42c33989b53052266350a45982273db

--- sm6150.mk
+++ sm6150.mk
@@ -147,6 +147,10 @@ PRODUCT_PACKAGES += \
     FM2 \
     qcom.fmradio
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti \
