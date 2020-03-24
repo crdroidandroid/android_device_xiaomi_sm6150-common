e01e63f693a6c2279ca0cff9c60ff65ea4fdf005
trpsl <trpsl755@gmail.com>
2020-03-24 14:10:26 +0100

    sm6150-common: Add Moto Dolby
    Values taken from OP9 Pro and modified by Sasha (helenius147 on Telegram)
    to work with Moto Dolby.
    
    - Additionally remove dax-default.xml from proprietary-files.txt
    - Improve audio_effects.xml changes
    - Move props from product.prop to vendor.prop
    - Dolby: We have 2 Output channels for Speakers
    - Disable AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP
    - ro.control_privapp_permissions=log -> force
    - Remove media_codecs_dolby_audio.xml, because we are not having backend for this.
    - Remove libswvqe Deps, they are not existing here for MotoDolby
    - Adapt for Enforcing A12.1 Sepolicy
    - Add # Dolby declarations for Sepolicy
    
    Co-authored-by: blankaf <realblankaf@gmail.com>
    Co-authored-by: MB <mb150996@gmail.com>
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>
    Co-authored-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I02e5c2c8b40ab5128c0f26e059406e892319d4c7

--- sm6150.mk
+++ sm6150.mk
@@ -143,6 +143,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
 
+# Dolby
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
