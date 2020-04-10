29208bebe94af2e45ba2b3287ccdc25270ec1b47
Midas Chien <midaschieh@google.com>
2020-04-10 21:12:56 +0800

    sm6150-common: rev graphics.composer to 2.4
    Move sunfish to composer 2.4 interface for the R api additions.
    
    Bug: 148836379
    Test:
    1. $adb shell lshal | grep 'composer'
        android.hardware.graphics.composer@2.4::IComposer/default
    
    2. $adb shell lshal --init-vintf
    <hal format="hidl">
       <name>android.hardware.graphics.composer</name>
       <transport>hwbinder</transport>
       <fqname>@2.4::IComposer/default</fqname>
    </hal>
    
    3. adb shell ps | grep 'composer'
    system ... android.hardware.graphics.composer@2.4-service-sm8150
    
    4. vts-tf > run vts -m VtsHalGraphicsComposerV2_4Target
    5. Check FOSS work
    
    Change-Id: Ic3e78b7ff04802120765b04f782021b405d2494f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -196,7 +196,7 @@ PRODUCT_COPY_FILES += \
 
 # Display/Graphics
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.3-service \
+    android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@2.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
