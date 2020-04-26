4bc8ee04a6b2acb9cb98829a224cb6445a72d334
Pranav Vashi <neobuddy89@gmail.com>
2020-04-27 01:02:26 +0530

    Revert "DNM: sm6150-common: Permissive selinux for bring up"
    * Cross your fingers!
    
    This reverts commit 5f3b5fda91283c5e5f655575b6f0165adf04d421.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,7 +41,6 @@ BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x880000 loop.max_part=7 cgroup.memory=nokmem,nosocket
 BOARD_KERNEL_CMDLINE +=  androidboot.vbmeta.avb_version=1.0
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
@@ -191,8 +190,6 @@ include device/qcom/sepolicy/sepolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-SELINUX_IGNORE_NEVERALLOWS := true
-SELINUX_IGNORE_NEVERALLOWS_ON_USER := true
 
 # WiFi
 BOARD_HOSTAPD_DRIVER := NL80211
