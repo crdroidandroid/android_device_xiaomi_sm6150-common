788b1f7294df7ddbf484715c2d9bcda1c0340ab4
Pranav Vashi <neobuddy89@gmail.com>
2020-04-26 12:09:40 +0530

    sm6150-common: Add gsi keys
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I3cefb4b78a6f34449e607ab12614c754fb0299e7

--- sm6150.mk
+++ sm6150.mk
@@ -7,6 +7,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
