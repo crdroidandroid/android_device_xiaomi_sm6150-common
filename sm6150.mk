b8089c33adf728030b85a3fc443549812f1693a8
ix5 <ix5@users.noreply.github.com>
2020-04-29 21:08:37 +0200

    sm6150-common: Add protobuf vendorcompat lib
    Our blobs are compiled against libprotobuf-full-cpp.so, but
    starting in R, Android is using a versioned naming approach,
    e.g. libprotobuf-cpp-full-3.9.1.so.
    
    See https://r.android.com/1109518
    
    The entry in public.libraries.txt is needed to allow the
    linker to find and use the lib outside the VNDK.
    
    See https://source.android.com/devices/tech/config/namespaces_libraries
    
    [wight554: added libprotobuf-cpp-lite as well]
    
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>
    Change-Id: I28222c89e1e07017867088492a726ee9d45d8e6f

--- sm6150.mk
+++ sm6150.mk
@@ -325,6 +325,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-vendorcompat \
+    libprotobuf-cpp-lite-vendorcompat
+
 # QTI
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
