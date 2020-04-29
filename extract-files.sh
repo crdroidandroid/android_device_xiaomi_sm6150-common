76d4dd32247f62acf755c1332462fa1efb2cb40f
Pranav Vashi <neobuddy89@gmail.com>
2020-04-29 15:53:35 +0530

    sm6150-common: Update blobs for CNE, ConfigStore and DPM
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ifa8cbc6ff9fe5faf7f5e94f7beb91629cbc24d59
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -88,4 +88,27 @@ fi
 
 COMMON_BLOB_ROOT="${LINEAGE_ROOT}/vendor/${VENDOR}/${DEVICE_COMMON}/proprietary"
 
+#
+# Fix product path
+#
+function fix_product_path () {
+    sed -i \
+        's/\/system\/framework\//\/system\/product\/framework\//g' \
+        "$COMMON_BLOB_ROOT"/"$1"
+}
+
+fix_product_path product/etc/permissions/vendor.qti.hardware.factory.xml
+
+#
+# Fix xml version
+#
+function fix_xml_version () {
+    sed -i \
+        's/xml version="2.0"/xml version="1.0"/' \
+        "$COMMON_BLOB_ROOT"/"$1"
+}
+
+fix_xml_version product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml
+fix_xml_version product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml
+
 "${MY_DIR}/setup-makefiles.sh"
