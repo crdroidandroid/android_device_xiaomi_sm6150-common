3b02a456292706f23016bd750d485d0b8bac5b76
Pranav Vashi <neobuddy89@gmail.com>
2020-05-21 23:43:29 +0530

    sm6150-common: Add switchbar to disable dynamic thermal
    * Disabled by default since all profile are set to 0 by default.
    
    Change-Id: I7fc8c5d82072a5388ebdd8d3ac679ec08c9cc023
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- parts/src/org/lineageos/settings/thermal/ThermalUtils.java
+++ parts/src/org/lineageos/settings/thermal/ThermalUtils.java
@@ -27,6 +27,9 @@ import org.lineageos.settings.utils.FileUtils;
 
 public final class ThermalUtils {
 
+    private static final String THERMAL_CONTROL = "thermal_control";
+    private static final String THERMAL_SERVICE = "thermal_service";
+
     protected static final int STATE_DEFAULT = 0;
     protected static final int STATE_BENCHMARK = 1;
     protected static final int STATE_BROWSER = 2;
@@ -34,7 +37,7 @@ public final class ThermalUtils {
     protected static final int STATE_DIALER = 4;
     protected static final int STATE_GAMING = 5;
     protected static final int STATE_STREAMING = 6;
-    private static final String THERMAL_CONTROL = "thermal_control";
+
     private static final String THERMAL_STATE_DEFAULT = "0";
     private static final String THERMAL_STATE_BENCHMARK = "10";
     private static final String THERMAL_STATE_BROWSER = "11";
@@ -58,9 +61,26 @@ public final class ThermalUtils {
         mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
     }
 
-    public static void startService(Context context) {
+    public static void initialize(Context context) {
+        if (isServiceEnabled(context))
+            startService(context);
+        else
+            setDefaultThermalProfile();
+    }
+
+    protected static void startService(Context context) {
         context.startServiceAsUser(new Intent(context, ThermalService.class),
                 UserHandle.CURRENT);
+        PreferenceManager.getDefaultSharedPreferences(context).edit().putString(THERMAL_SERVICE, "true").apply();
+    }
+
+    protected static void stopService(Context context) {
+        context.stopService(new Intent(context, ThermalService.class));
+        PreferenceManager.getDefaultSharedPreferences(context).edit().putString(THERMAL_SERVICE, "false").apply();
+    }
+
+    protected static boolean isServiceEnabled(Context context) {
+        return Boolean.valueOf(PreferenceManager.getDefaultSharedPreferences(context).getString(THERMAL_SERVICE, "false"));
     }
 
     private void writeValue(String profiles) {
@@ -132,7 +152,7 @@ public final class ThermalUtils {
         return state;
     }
 
-    protected void setDefaultThermalProfile() {
+    protected static void setDefaultThermalProfile() {
         FileUtils.writeLine(THERMAL_SCONFIG, THERMAL_STATE_DEFAULT);
     }
 
