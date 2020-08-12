12bb11b7183c997ba5a44b45a2fe88e5e3b1f8e9
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    sm6150-common: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    xNombre: Maybe in future I will implement camera hints, for now just stop
    camera libs moaning.
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -275,7 +275,8 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr \
+    libqti-perfd-client
 
 # QTI
 PRODUCT_PACKAGES += \
