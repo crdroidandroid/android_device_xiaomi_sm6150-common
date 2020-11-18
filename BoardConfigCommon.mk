69ecb2bddda9ade36e014594e667f87c2c4f3bee
Pranav Vashi <neobuddy89@gmail.com>
2020-11-19 00:27:07 +0530

    sm6150-common: Fix DT2W on stock PowerHAL
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I9a044c5fd26e433f0d380a7679f5076d5749e9ef

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -180,6 +180,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_COPY_OUT_PRODUCT := product
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Power
+TARGET_POWER_SET_FEATURE_LIB := libpower_feature.xiaomi_sm6150
+
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
---
+++ power/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libpower_feature.xiaomi_sm6150
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_VENDOR_MODULE := true
+
+LOCAL_SRC_FILES := power-feature.c
+
+LOCAL_SHARED_LIBRARIES := libutils
+
+LOCAL_HEADER_LIBRARIES := libhardware_headers qti_powerhal_headers
+
+include $(BUILD_STATIC_LIBRARY)
