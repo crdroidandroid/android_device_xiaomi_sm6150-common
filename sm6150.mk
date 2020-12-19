989600cd4ae2303661ff56306036029cb3f16277
Pranav Vashi <neobuddy89@gmail.com>
2020-12-20 01:40:49 +0530

    sm6150-common: Update RIL packages
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -370,9 +370,10 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.1 \
     android.hardware.radio@1.4 \
     android.hardware.secure_element@1.0 \
+    libavservices_minijail.vendor \
     libjson \
     libprotobuf-cpp-full \
     librmnetctl \
