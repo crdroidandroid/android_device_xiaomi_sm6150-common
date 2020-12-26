4d3be3f23a7b06c7b1629bff7ac131f7ad368c76
Pranav Vashi <neobuddy89@gmail.com>
2020-12-26 15:38:15 +0530

    sm6150-common: Use single fstab
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -98,11 +98,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab_dynamic.qcom
 LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.persist
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.persist
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
