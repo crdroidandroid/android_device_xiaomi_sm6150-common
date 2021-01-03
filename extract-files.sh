e97462303928083589c2287fb885735c8471a067
Pranav Vashi <neobuddy89@gmail.com>
2021-01-03 13:17:12 +0530

    sm6150-common: Update blobs from RKQ1.200826.002-V12.1.3.0.RGHCNXM
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -59,12 +59,24 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+    vendor/bin/mlipayd@1.1 | vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so )
+        "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
+    ;;
+    system_ext/lib64/libwfdnative.so | vendor/lib64/libgoodixhwfingerprint.so )
+        "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
+    ;;
     vendor/etc/camera/camxoverridesettings.txt )
         sed -i "s|0x10080|0|g" "${2}"
         sed -i "s|0x1F|0x0|g" "${2}"
     ;;
-    vendor/lib64/hw/camera.qcom.so)
-        patchelf --add-needed "libc++demangle.so" "${2}"
+    vendor/etc/init/vendor.sensors.qti.rc )
+        echo "    disabled" >> "${2}"
+    ;;
+    system_ext/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | system_ext/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml )
+        sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
+    ;;
+    lib/libcameraservice.so )
+        "${PATCHELF}" --add-needed "libmedia_jni_shim.so" "${2}"
     ;;
     esac
 }
