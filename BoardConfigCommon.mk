daa5b75b228ee3df8952a7c2faa366c366d540ca
arco <arco68@gmail.com>
2021-01-20 02:16:47 +0700

    sm6150-common: Silence clang warnings during kernel compile
    clang-11: warning: argument unused during compilation:
    '-fuse-ld=lld' [-Wunused-command-line-argument]
    
    Change-Id: I1ebf0fb665704424a8ad3a375c546c6cca44dc0b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -90,6 +90,8 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
