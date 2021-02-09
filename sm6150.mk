e3cf5410284840f907f0feea6f9ebd4e7a2dd19a
Pranav Vashi <neobuddy89@gmail.com>
2021-02-09 13:33:15 +0530

    sm6150-common: Uprev radio and secure element interface
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -349,8 +349,8 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.1 \
-    android.hardware.radio@1.4 \
-    android.hardware.secure_element@1.0 \
+    android.hardware.radio@1.5 \
+    android.hardware.secure_element@1.1 \
     libjson \
     libprotobuf-cpp-full \
     librmnetctl \
