c96d88c1df07d802a5082c84882a0d134234b0a7
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-04-11 15:06:22 +0530

    sm6150-common: Enable power HAL interaction boosting

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,6 +135,7 @@ TARGET_BOARD_PLATFORM := sm6150
 
 # Power
 TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
+TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
