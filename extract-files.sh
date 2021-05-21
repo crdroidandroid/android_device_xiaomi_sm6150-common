d80bf5e476ade9bada80bc1c1fae33579293d3c6
Pranav Vashi <neobuddy89@gmail.com>
2021-05-21 23:21:59 +0530

    sm6150-common: Update proprietary files list
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -71,7 +71,10 @@ function blob_fixup() {
         sed -i "s|0x1F|0x0|g" "${2}"
     ;;
     vendor/etc/init/vendor.sensors.qti.rc )
-    echo "    disabled" >> "${2}"
+        echo "    disabled" >> "${2}"
+    ;;
+    system_ext/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | system_ext/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml )
+        sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
     ;;
     esac
 }
