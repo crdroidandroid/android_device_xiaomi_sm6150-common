99f5283a96da944a3f4d4731fe6406308a8563a7
Pranav Vashi <neobuddy89@gmail.com>
2021-05-26 16:57:38 +0530

    sm6150-common: Fine tune post boot script
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -83,12 +83,14 @@ function configure_read_ahead_kb_values() {
         echo 128 > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb
         for dm in $dmpts; do
             echo 128 > $dm
+            echo "Configured read_ahead_kb to 128 at " $dm
         done
     else
         echo 512 > /sys/block/mmcblk0/bdi/read_ahead_kb
         echo 512 > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb
         for dm in $dmpts; do
             echo 512 > $dm
+            echo "Configured read_ahead_kb to 512 at " $dm
         done
     fi
 }
@@ -116,60 +118,64 @@ function enable_swap() {
 }
 
 function configure_memory_parameters() {
-    MemTotalStr=`cat /proc/meminfo | grep MemTotal`
-    MemTotal=${MemTotalStr:16:8}
+    if [ -d /sys/module/lowmemorykiller ]; then
+        MemTotalStr=`cat /proc/meminfo | grep MemTotal`
+        MemTotal=${MemTotalStr:16:8}
 
-    # Read adj series and set adj threshold for PPR and ALMK.
-    # This is required since adj values change from framework to framework.
-    adj_series=`cat /sys/module/lowmemorykiller/parameters/adj`
-    adj_1="${adj_series#*,}"
-    set_almk_ppr_adj="${adj_1%%,*}"
+        # Read adj series and set adj threshold for PPR and ALMK.
+        # This is required since adj values change from framework to framework.
+        adj_series=`cat /sys/module/lowmemorykiller/parameters/adj`
+        adj_1="${adj_series#*,}"
+        set_almk_ppr_adj="${adj_1%%,*}"
 
-    # PPR and ALMK should not act on HOME adj and below.
-    # Normalized ADJ for HOME is 6. Hence multiply by 6
-    # ADJ score represented as INT in LMK params, actual score can be in decimal
-    # Hence add 6 considering a worst case of 0.9 conversion to INT (0.9*6).
-    # For uLMK + Memcg, this will be set as 6 since adj is zero.
-    set_almk_ppr_adj=$(((set_almk_ppr_adj * 6) + 6))
-    echo $set_almk_ppr_adj > /sys/module/lowmemorykiller/parameters/adj_max_shift
+        # PPR and ALMK should not act on HOME adj and below.
+        # Normalized ADJ for HOME is 6. Hence multiply by 6
+        # ADJ score represented as INT in LMK params, actual score can be in decimal
+        # Hence add 6 considering a worst case of 0.9 conversion to INT (0.9*6).
+        # For uLMK + Memcg, this will be set as 6 since adj is zero.
+        set_almk_ppr_adj=$(((set_almk_ppr_adj * 6) + 6))
+        echo $set_almk_ppr_adj > /sys/module/lowmemorykiller/parameters/adj_max_shift
 
-    # Calculate vmpressure_file_min as below & set for 64 bit:
-    # vmpressure_file_min = last_lmk_bin + (last_lmk_bin - last_but_one_lmk_bin)
-    minfree_series=`cat /sys/module/lowmemorykiller/parameters/minfree`
-    minfree_1="${minfree_series#*,}" ; rem_minfree_1="${minfree_1%%,*}"
-    minfree_2="${minfree_1#*,}" ; rem_minfree_2="${minfree_2%%,*}"
-    minfree_3="${minfree_2#*,}" ; rem_minfree_3="${minfree_3%%,*}"
-    minfree_4="${minfree_3#*,}" ; rem_minfree_4="${minfree_4%%,*}"
-    minfree_5="${minfree_4#*,}"
+        # Calculate vmpressure_file_min as below & set for 64 bit:
+        # vmpressure_file_min = last_lmk_bin + (last_lmk_bin - last_but_one_lmk_bin)
+        minfree_series=`cat /sys/module/lowmemorykiller/parameters/minfree`
+        minfree_1="${minfree_series#*,}" ; rem_minfree_1="${minfree_1%%,*}"
+        minfree_2="${minfree_1#*,}" ; rem_minfree_2="${minfree_2%%,*}"
+        minfree_3="${minfree_2#*,}" ; rem_minfree_3="${minfree_3%%,*}"
+        minfree_4="${minfree_3#*,}" ; rem_minfree_4="${minfree_4%%,*}"
+        minfree_5="${minfree_4#*,}"
 
-    vmpres_file_min=$((minfree_5 + (minfree_5 - rem_minfree_4)))
-    echo $vmpres_file_min > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
-    if [ $MemTotal -lt 3145728 ]; then
-        echo "18432,23040,27648,32256,100640,120640" > /sys/module/lowmemorykiller/parameters/minfree
-    elif [ $MemTotal -lt 4194304 ]; then
-        echo "18432,23040,27648,38708,120640,144768" > /sys/module/lowmemorykiller/parameters/minfree
-    elif [ $MemTotal -lt 6291456 ]; then
-        echo "18432,23040,27648,64512,165888,225792" > /sys/module/lowmemorykiller/parameters/minfree
-    else
-        echo "18432,23040,27648,96768,276480,362880" > /sys/module/lowmemorykiller/parameters/minfree
+        vmpres_file_min=$((minfree_5 + (minfree_5 - rem_minfree_4)))
+        echo $vmpres_file_min > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
+        if [ $MemTotal -lt 3145728 ]; then
+            echo "18432,23040,27648,32256,100640,120640" > /sys/module/lowmemorykiller/parameters/minfree
+        elif [ $MemTotal -lt 4194304 ]; then
+            echo "18432,23040,27648,38708,120640,144768" > /sys/module/lowmemorykiller/parameters/minfree
+        elif [ $MemTotal -lt 6291456 ]; then
+            echo "18432,23040,27648,64512,165888,225792" > /sys/module/lowmemorykiller/parameters/minfree
+        else
+            echo "18432,23040,27648,96768,276480,362880" > /sys/module/lowmemorykiller/parameters/minfree
+        fi
+
+        # Enable adaptive LMK for all targets &
+        # use Google default LMK series for all 64-bit targets >=2GB.
+        echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
+
+        # Enable oom_reaper
+        if [ -f /sys/module/lowmemorykiller/parameters/oom_reaper ]; then
+            echo 1 > /sys/module/lowmemorykiller/parameters/oom_reaper
+        fi
+
+        #Set PPR parameters for all other targets.
+        if [ -d /sys/module/process_reclaim ]; then
+            echo $set_almk_ppr_adj > /sys/module/process_reclaim/parameters/min_score_adj
+            echo 0 > /sys/module/process_reclaim/parameters/enable_process_reclaim
+            echo 50 > /sys/module/process_reclaim/parameters/pressure_min
+            echo 70 > /sys/module/process_reclaim/parameters/pressure_max
+            echo 30 > /sys/module/process_reclaim/parameters/swap_opt_eff
+            echo 512 > /sys/module/process_reclaim/parameters/per_swap_size
+        fi
     fi
-
-    # Enable adaptive LMK for all targets &
-    # use Google default LMK series for all 64-bit targets >=2GB.
-    echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
-
-    # Enable oom_reaper
-    if [ -f /sys/module/lowmemorykiller/parameters/oom_reaper ]; then
-        echo 1 > /sys/module/lowmemorykiller/parameters/oom_reaper
-    fi
-
-    #Set PPR parameters for all other targets.
-    echo $set_almk_ppr_adj > /sys/module/process_reclaim/parameters/min_score_adj
-    echo 0 > /sys/module/process_reclaim/parameters/enable_process_reclaim
-    echo 50 > /sys/module/process_reclaim/parameters/pressure_min
-    echo 70 > /sys/module/process_reclaim/parameters/pressure_max
-    echo 30 > /sys/module/process_reclaim/parameters/swap_opt_eff
-    echo 512 > /sys/module/process_reclaim/parameters/per_swap_size
 
     # Set allocstall_threshold to 0 for all targets.
     # Set swappiness to 100 for all targets
@@ -198,19 +204,22 @@ if [ -f /sys/devices/soc0/soc_id ]; then
 else
         soc_id=`cat /sys/devices/system/soc/soc0/id`
 fi
+echo "soc_id is " $soc_id
 
 case "$soc_id" in
     "355" | "369" | "377" | "380" | "384" )
 
     # Core control parameters on silver
-    echo 0 0 0 0 1 1 > /sys/devices/system/cpu/cpu0/core_ctl/not_preferred
-    echo 4 > /sys/devices/system/cpu/cpu0/core_ctl/min_cpus
-    echo 60 > /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres
-    echo 40 > /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres
-    echo 100 > /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms
-    echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster
-    echo 8 > /sys/devices/system/cpu/cpu0/core_ctl/task_thres
-    echo 0 > /sys/devices/system/cpu/cpu6/core_ctl/enable
+    if [ -d /sys/devices/system/cpu/cpu0/core_ctl ]; then
+        echo 0 0 0 0 1 1 > /sys/devices/system/cpu/cpu0/core_ctl/not_preferred
+        echo 4 > /sys/devices/system/cpu/cpu0/core_ctl/min_cpus
+        echo 60 > /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres
+        echo 40 > /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres
+        echo 100 > /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms
+        echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster
+        echo 8 > /sys/devices/system/cpu/cpu0/core_ctl/task_thres
+        echo 0 > /sys/devices/system/cpu/cpu6/core_ctl/enable
+    fi
 
     # Setting b.L scheduler parameters
     # default sched up and down migrate values are 90 and 85
@@ -266,6 +275,7 @@ case "$soc_id" in
             echo 250 > $cpubw/bw_hwmon/up_scale
             echo 1600 > $cpubw/bw_hwmon/idle_mbps
             echo 50 > $cpubw/polling_interval
+            echo "Configured cpubw node at " $cpubw
         done
 
         for llccbw in $device/*cpu-llcc-ddr-bw/devfreq/*cpu-llcc-ddr-bw
@@ -281,6 +291,25 @@ case "$soc_id" in
             echo 250 > $llccbw/bw_hwmon/up_scale
             echo 1600 > $llccbw/bw_hwmon/idle_mbps
             echo 40 > $llccbw/polling_interval
+            echo "Configured llccbw node at " $llccbw
+        done
+
+        # Gold L3 ratio ceil
+        echo 4000 > /sys/class/devfreq/soc:qcom,cpu6-cpu-l3-lat/mem_latency/ratio_ceil
+
+        # Enable cdspl3 governor for L3 cdsp nodes
+        for l3cdsp in $device/*cdsp-cdsp-l3-lat/devfreq/*cdsp-cdsp-l3-lat
+        do
+            echo "cdspl3" > $l3cdsp/governor
+            echo "Configured l3cdsp node at " $l3cdsp
+        done
+
+        # Enable compute governor for gold latfloor
+        for latfloor in $device/*cpu*-ddr-latfloor*/devfreq/*cpu-ddr-latfloor*
+        do
+            echo "compute" > $latfloor/governor
+            echo 10 > $latfloor/polling_interval
+            echo "Configured latfloor node at " $latfloor
         done
     done
 
@@ -305,14 +334,16 @@ case "$soc_id" in
     "365" | "366" )
 
     # Core control parameters on silver
-    echo 0 0 0 0 1 1 > /sys/devices/system/cpu/cpu0/core_ctl/not_preferred
-    echo 4 > /sys/devices/system/cpu/cpu0/core_ctl/min_cpus
-    echo 60 > /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres
-    echo 40 > /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres
-    echo 100 > /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms
-    echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster
-    echo 8 > /sys/devices/system/cpu/cpu0/core_ctl/task_thres
-    echo 0 > /sys/devices/system/cpu/cpu6/core_ctl/enable
+    if [ -d /sys/devices/system/cpu/cpu0/core_ctl ]; then
+        echo 0 0 0 0 1 1 > /sys/devices/system/cpu/cpu0/core_ctl/not_preferred
+        echo 4 > /sys/devices/system/cpu/cpu0/core_ctl/min_cpus
+        echo 60 > /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres
+        echo 40 > /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres
+        echo 100 > /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms
+        echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster
+        echo 8 > /sys/devices/system/cpu/cpu0/core_ctl/task_thres
+        echo 0 > /sys/devices/system/cpu/cpu6/core_ctl/enable
+    fi
 
     # Setting b.L scheduler parameters
     # default sched up and down migrate values are 71 and 65
@@ -367,6 +398,7 @@ case "$soc_id" in
             echo 250 > $cpubw/bw_hwmon/up_scale
             echo 1600 > $cpubw/bw_hwmon/idle_mbps
             echo 50 > $cpubw/polling_interval
+            echo "Configured cpubw node at " $cpubw
         done
 
         for llccbw in $device/*cpu-llcc-ddr-bw/devfreq/*cpu-llcc-ddr-bw
@@ -382,6 +414,7 @@ case "$soc_id" in
             echo 250 > $llccbw/bw_hwmon/up_scale
             echo 1600 > $llccbw/bw_hwmon/idle_mbps
             echo 40 > $llccbw/polling_interval
+            echo "Configured llccbw node at " $llccbw
         done
 
         for npubw in $device/*npu-npu-ddr-bw/devfreq/*npu-npu-ddr-bw
@@ -399,6 +432,25 @@ case "$soc_id" in
             echo 0 > $npubw/bw_hwmon/idle_mbps
             echo 40 > $npubw/polling_interval
             echo 0 > /sys/devices/virtual/npu/msm_npu/pwr
+            echo "Configured npubw node at " $npubw
+        done
+
+        # Gold L3 ratio ceil
+        echo 4000 > /sys/class/devfreq/soc:qcom,cpu6-cpu-l3-lat/mem_latency/ratio_ceil
+
+        # Enable cdspl3 governor for L3 cdsp nodes
+        for l3cdsp in $device/*cdsp-cdsp-l3-lat/devfreq/*cdsp-cdsp-l3-lat
+        do
+            echo "cdspl3" > $l3cdsp/governor
+            echo "Configured l3cdsp node at " $l3cdsp
+        done
+
+        # Enable compute governor for gold latfloor
+        for latfloor in $device/*cpu*-ddr-latfloor*/devfreq/*cpu-ddr-latfloor*
+        do
+            echo "compute" > $latfloor/governor
+            echo 10 > $latfloor/polling_interval
+            echo "Configured latfloor node at " $latfloor
         done
     done
 
