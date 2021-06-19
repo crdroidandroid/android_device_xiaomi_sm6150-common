8e9a67672e0ae06fe204e48e135bc94b58f5c594
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-19 11:46:02 +0200

    sm6150-common: vibrator: Rebrand to Xiaomi SM6150
    Change-Id: Icd0ec7c857574caec085751253fafa39be065d0d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -410,7 +410,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi_sm6150
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)//etc/excluded-input-devices.xml
