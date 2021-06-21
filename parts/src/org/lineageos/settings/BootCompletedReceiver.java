755bcc7fc4c7be4b879b3d85f50239c3d63335b3
Nauval Rizky <enuma.alrizky@gmail.com>
2021-06-21 10:19:21 +0700

    sm6150-common: parts: Introduce haptic feedback level adjustment
    Since our kernel already expose adjustable level, lets add
    a simple UI for it.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I35577682f97e4aad880e455423bf51f6cbe1641b

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -25,6 +25,7 @@ import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.haptic.HapticUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -36,5 +37,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize(context);
         DozeUtils.onBootCompleted(context);
+        HapticUtils.restoreLevel(context);
     }
 }
