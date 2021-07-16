612be8e9ce9dbcb40647f397da5663e9573ac67e
xNombre <kartapolska@gmail.com>
2021-07-16 23:00:29 +0200

    sm6150-common: Update config.fs from LA.UM.9.1.r1-10600-SMxxx0.0
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Change-Id: Ia300e498be2ba938ab6d690fae23a93b8c5c613a

--- configs/config.fs
+++ configs/config.fs
@@ -71,19 +71,19 @@ caps: NET_BIND_SERVICE
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: NET_BIND_SERVICE SYS_BOOT
+caps: NET_BIND_SERVICE
 
 [system/vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: NET_BIND_SERVICE SYS_BOOT
+caps: NET_BIND_SERVICE
 
 [system/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: NET_BIND_SERVICE SYS_BOOT
+caps: NET_BIND_SERVICE
 
 [vendor/bin/pd-mapper]
 mode: 0755
