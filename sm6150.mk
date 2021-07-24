87df8cfc3031294eb4484b37fda8182631063906
xNombre <kartapolska@gmail.com>
2021-07-24 21:54:40 +0200

    sm6150-common: Build MiuiCamera as default camera app
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Change-Id: Ic197fd241fd4fa5b0924ede9d397c8800d9daf52

--- sm6150.mk
+++ sm6150.mk
@@ -106,6 +106,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+$(call inherit-product, vendor/miuicamera/config.mk)
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
