6f3ae833e75415438fb3cfc844c6c56ee4af72a6
xNombre <kartapolska@gmail.com>
2021-08-23 22:55:38 +0200

    sm6150-common: Implement camera hints handling
    Camera uses CHI interface to communicate with perf HAL. Libperfmgr can't
    handle these hints natively so a special link between each other is needed.
    
    CHI sources revealed that it is sending packet of per-device hints, these
    remain useless for us because Google hints will be used. For this reason
    I've decided to use packet size as an identifier of hint type, this turned
    out to be very accurate. I was able to empirically match each packet size
    with corresponding action. This was then implemented and wired up to google
    power HAL.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Change-Id: Ibe39ea3c0d78cc277b47ad7e3c12af0d01f5de67

--- libqti-perfd-client/client.cpp
+++ libqti-perfd-client/client.cpp
@@ -1,10 +1,139 @@
+#define LOG_TAG "libqti-perfd-client"
+
 #include <stdint.h>
+#include <shared_mutex>
+#include <string>
+
+#include <android/binder_manager.h>
+#include <android/log.h>
+#include <binder/IServiceManager.h>
+
+#include <aidl/android/hardware/power/IPower.h>
+
+using aidl::android::hardware::power::Boost;
+using aidl::android::hardware::power::IPower;
+using aidl::android::hardware::power::Mode;
+using IPowerAidl = aidl::android::hardware::power::IPower;
 
 namespace android {
-    extern "C" void perf_get_feedback() {}
-    extern "C" void perf_hint() {}
-    extern "C" void perf_lock_acq() {}
-    extern "C" void perf_lock_cmd() {}
-    extern "C" void perf_lock_rel() {}
-    extern "C" void perf_lock_use_profile() {}
+class PowerAidl {
+    std::shared_ptr<IPowerAidl> power_hal_aidl_;
+
+  public:
+    PowerAidl() {
+        const std::string kInstance = std::string(IPower::descriptor) + "/default";
+        ndk::SpAIBinder power_binder =
+                ndk::SpAIBinder(AServiceManager_getService(kInstance.c_str()));
+
+        if (power_binder.get() == nullptr) {
+            ALOGE("Cannot get Power Hal Binder");
+            return;
+        }
+
+        power_hal_aidl_ = IPower::fromBinder(power_binder);
+
+        if (power_hal_aidl_ == nullptr) {
+            ALOGE("Cannot get Power Hal AIDL");
+        }
+    }
+
+    void setMode(Mode hint, bool enabled) {
+        if (power_hal_aidl_ == nullptr) {
+            ALOGE("power_hal_aidl_ is null!");
+            return;
+        }
+
+        auto ret = power_hal_aidl_->setMode(hint, enabled);
+
+        if (!ret.isOk()) {
+            ALOGE("Set mode %s to %d failed!", toString(hint).c_str(), enabled);
+        }
+    }
+    void setBoost(Boost hint, int32_t duration) {
+        if (power_hal_aidl_ == nullptr) {
+            ALOGE("power_hal_aidl_ is null!");
+            return;
+        }
+
+        auto ret = power_hal_aidl_->setBoost(hint, duration);
+
+        if (!ret.isOk()) {
+            ALOGE("Set boost %s for %dms failed!", toString(hint).c_str(), duration);
+        }
+    }
+};
+
+static PowerAidl poweraidl = PowerAidl();
+static int handleNum = 0;
+
+extern "C" void perf_get_feedback() {}
+extern "C" void perf_hint() {}
+extern "C" void perf_lock_cmd() {}
+extern "C" void perf_lock_use_profile() {}
+
+extern "C" int perf_lock_acq(int handle, int duration_ms, int params[], int size) {
+    int ret;
+
+    ALOGI("perf_lock_acq: handle: %d, duration_ms: %d, params[0]: %d, size: %d", handle,
+          duration_ms, params[0], size);
+
+    switch (size) {
+        case 14: /* CAMERA_LAUNCH */
+            poweraidl.setBoost(Boost::CAMERA_LAUNCH, 0);
+            ALOGI("perf_lock_acq: CAMERA_LAUNCH");
+            break;
+
+        case 20: /* CAMERA_SHOT */
+            poweraidl.setBoost(Boost::CAMERA_SHOT, 0);
+            ALOGI("perf_lock_acq: CAMERA_SHOT");
+            break;
+
+        case 22: /* Streaming 4k */
+            poweraidl.setMode(Mode::CAMERA_STREAMING_HIGH, true);
+            ALOGI("perf_lock_acq: CAMERA_STREAMING_HIGH");
+            break;
+
+        case 30: /* Streaming 1080p60fps */
+            poweraidl.setMode(Mode::CAMERA_STREAMING_MID, true);
+            ALOGI("perf_lock_acq: CAMERA_STREAMING_MID");
+            break;
+
+        case 26: /* Streaming 720p/1080p30fps */
+            poweraidl.setMode(Mode::CAMERA_STREAMING_LOW, true);
+            ALOGI("perf_lock_acq: CAMERA_STREAMING_LOW");
+            break;
+
+        case 10: /* CAMERA_CLOSE - release all handles */
+            // Returning 0 makes chi cry but prevents refCount from growing indefinitely
+            ret = 0;
+            handleNum = 0;
+
+            poweraidl.setMode(Mode::CAMERA_STREAMING_LOW, false);
+            poweraidl.setMode(Mode::CAMERA_STREAMING_MID, false);
+            poweraidl.setMode(Mode::CAMERA_STREAMING_HIGH, false);
+
+            ALOGI("perf_lock_acq: CAMERA_CLOSE");
+            goto out;
+            break;
+
+        default: /* Not implemented */
+            return 0;
+            break;
+    }
+
+    if (handle == 0) {
+        ret = ++handleNum;
+    } else {
+        ret = handle;
+    }
+
+out:
+    ALOGI("perf_lock_acq: returning handle val: %d", ret);
+    return ret;
 }
+
+extern "C" int perf_lock_rel(int handle) {
+    ALOGI("perf_lock_rel: handle: %d", handle);
+    return 0;  // success
+}
+}  // namespace android
