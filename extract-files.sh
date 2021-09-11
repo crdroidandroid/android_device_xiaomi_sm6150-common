adebae6b348ac87808747e4382176707648e7a41
Pranav Vashi <neobuddy89@gmail.com>
2021-09-11 10:36:50 +0530

    sm6150-common: Update wifi display stack from LA.UM.9.1.r1-10900-SMxxx0.0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -63,7 +63,7 @@ function blob_fixup() {
     vendor/bin/mlipayd@1.1 | vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so )
         "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
     ;;
-    system_ext/lib/libwfdnative.so | system_ext/lib64/libwfdnative.so | vendor/lib64/libgoodixhwfingerprint.so )
+    vendor/lib64/libgoodixhwfingerprint.so )
         "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
     ;;
     vendor/etc/camera/camxoverridesettings.txt )
