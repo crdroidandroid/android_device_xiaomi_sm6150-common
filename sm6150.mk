17402d5a6cf513b2573c459d457cdb7034a88690
Pranav Vashi <neobuddy89@gmail.com>
2021-09-17 17:56:35 +0530

    Revert "sm6150-common: vendor-common: Add QTI health implementation"
    This reverts commit 8f645791ca5c5845c81883607944f4aac97870de.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -229,7 +229,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-qti:64 \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
 # IFAA manager
