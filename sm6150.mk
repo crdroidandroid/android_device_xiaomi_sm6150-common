2828ceb0aa75e02a84c7b80e1f3bf0d1461c848e
ItsVixano <vixanonew@gmail.com>
2021-10-12 21:34:06 +0530

    [DNM]sm6150-common: for the sake of snow cone
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -91,6 +91,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio@2.0-impl \
+		android.hardware.bluetooth@1.0.vendor \
+		bt_stack.conf \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
