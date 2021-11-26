43b330ed1236098ee6ce7bec3ab72db14a46ffc4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-26 21:58:03 +0530

    sm6150-common: Drop soundtrigger HIDL impl
     * breaks google voice match with pixelpropsutils as
       we don't have hotword enrollment support
    
    Change-Id: I213626f9729e5ff47ce7e6c990d605ed669b97e3

--- sm6150.mk
+++ sm6150.mk
@@ -41,8 +41,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio-impl \
-    android.hardware.soundtrigger@2.3-impl
+    android.hardware.bluetooth.audio-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
