465d2de925635ff3861ce8bdca7c0ff7bfc57d17
SakilMondal <mondalsakil@gmail.com>
2021-11-26 18:26:36 -0500

    sm6150-common: Drop non-functional soundtrigger
    * Fixes google assistant voice match in android 12
    
    Change-Id: I21100e2f368ec0ddf3b546372b241d85bb698747

--- sm6150.mk
+++ sm6150.mk
@@ -43,7 +43,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl 
+    android.hardware.bluetooth.audio@2.0-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
