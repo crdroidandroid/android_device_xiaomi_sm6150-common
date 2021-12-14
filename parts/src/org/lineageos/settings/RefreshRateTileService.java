76ca5c4da6fc5811fc8ad92e223376321e018e79
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-14 15:57:44 +0700

    sm6150-common: parts: Sort refresh rate cycle in natural order
    Change-Id: Ib993f8fa1be996d161e92168221f15716695754c

--- parts/src/org/lineageos/settings/RefreshRateTileService.java
+++ parts/src/org/lineageos/settings/RefreshRateTileService.java
@@ -24,6 +24,8 @@ import android.service.quicksettings.TileService;
 import android.view.Display;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 
@@ -50,6 +52,9 @@ public class RefreshRateTileService extends TileService {
                 m.getPhysicalHeight() == mode.getPhysicalHeight()) {
                 availableRates.add(rate);
             }
+        }
+        if (!availableRates.isEmpty()) {
+            availableRates.sort(Comparator.naturalOrder());
         }
         syncFromSettings();
     }
