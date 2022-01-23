55056a94bac5136cf3252aad5f8ca3c9297379d5
Zinadin Zidan <zidan44@pixelexperience.org>
2022-01-24 00:33:52 +0600

    sm6150-common: Update from mojito/sweet miui13 A12 release
    * Blobs: mojito-user-12-SKQ1.210908.001-V13.0.3.0.SKGMIXM-release-keys
    * Media-Conf: sweet-user-12-SKQ1.210908.001-V13.0.2.0.SKFMIXM-release-keys
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I510e28466cafc921d49ea58d0a7070bd0486631e
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -16,10 +16,6 @@ $(call inherit-product, vendor/xiaomi/sm6150-common/sm6150-common-vendor.mk)
 # Camera
 $(call inherit-product-if-exists, vendor/miuicamera/config.mk)
 
-# Additional native libraries
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
-
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
@@ -389,10 +385,6 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
-    $(LOCAL_PATH)/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
