a87c100f68ab791cb001be69455b0cc46629b537
Razziell <razziell@ya.ru>
2022-02-19 20:39:37 +0300

    davinci: Enable freeform windows

--- sm6150.mk
+++ sm6150.mk
@@ -149,6 +149,10 @@ PRODUCT_PACKAGES += \
     libqcomfm_jni \
     qcom.fmradio
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.zram:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.zram
