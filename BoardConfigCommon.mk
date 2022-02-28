1f2358d0ab4586da1c2acaa8a1b0bd62ffb0a0c6
Andrzej Perczak <linux@andrzejperczak.com>
2022-03-01 00:51:16 +0100

    sm6150-common: Move system props to product
    This is more proper place.
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Change-Id: I7f011c1a4e2da4e5ead072a2cf98ddac4aeca8c3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -120,7 +120,7 @@ TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
-TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # QCOM
