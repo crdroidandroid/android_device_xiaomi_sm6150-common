53e9e91516dc7e0907585d7dd4f2a1fc75c079f9
johnmart19 <johnivan19999@gmail.com>
2022-03-30 19:48:25 +0300

    sm6150-common: Build missing bluetooth libs
    E a2dp_offload: open_a2dp_sink: DLOPEN failed for libbthost_if_sink.so

--- sm6150.mk
+++ sm6150.mk
@@ -72,6 +72,9 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
+    libbthost_if \
+    libbthost_if.vendor \
+    libldacBT_bco.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
