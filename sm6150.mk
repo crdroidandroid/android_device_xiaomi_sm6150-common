086a904045d7e4953658a9ef797e78b493edbc9a
Pranav Vashi <neobuddy89@gmail.com>
2022-04-06 01:35:02 +0530

    sm6150-common: Enable Quick Tap
    Change-Id: Ifa5a10580d5f83c2c5cb01b06e1dcd319eef4e6c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+TARGET_SUPPORTS_QUICK_TAP := true
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
