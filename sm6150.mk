dd8cc527a4c3ba237ed69ae390aec9657a2bc711
Laker <lakerman1987@gmail.com>
2022-04-09 09:45:12 +0300

    davinci: build drm 1.4 & nfc vendor hidl

--- sm6150.mk
+++ sm6150.mk
@@ -136,8 +136,8 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Fingerprint
 PRODUCT_COPY_FILES += \
