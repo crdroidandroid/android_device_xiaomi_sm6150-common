03529154ff63194830e8725b7a1e17749e0db903
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-14 01:48:37 +0200

    sm6150-common: Move to common IFAAService
    Change-Id: I71bfdacc4626d75d56aa12ec384f0f116d6d6ff2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -243,11 +243,7 @@ PRODUCT_COPY_FILES += \
 
 # IFAA manager
 PRODUCT_PACKAGES += \
-    IFAAService \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+    IFAAService
 
 # IMS
 PRODUCT_PACKAGES += \
