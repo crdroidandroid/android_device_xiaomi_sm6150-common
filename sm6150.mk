47337aa63e82b7985aa8e60435d25ae0bacf678b
Andrzej Perczak <linux@andrzejperczak.com>
2022-04-21 21:27:23 +0200

    sm6150-common: Enable Quick Tap
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>

--- sm6150.mk
+++ sm6150.mk
@@ -282,6 +282,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
+# Quick Tap
+TARGET_SUPPORTS_QUICK_TAP := true
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.6.vendor \
