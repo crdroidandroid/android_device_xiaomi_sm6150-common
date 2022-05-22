2140babc73de08b4a80a0790c58d5ec2b1e2faa4
Pranav Vashi <neobuddy89@gmail.com>
2022-05-22 09:00:36 +0530

    sm6150-common: Exclude AudioFx
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ia2580204ca48555759e5cb3e820d237749165ad5

--- sm6150.mk
+++ sm6150.mk
@@ -132,6 +132,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
+TARGET_EXCLUDES_AUDIOFX := true
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
