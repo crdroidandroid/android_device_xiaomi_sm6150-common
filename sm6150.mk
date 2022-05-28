91f1c6d67113e6b322c95ad7002c660bd9c9e257
Pranav Vashi <neobuddy89@gmail.com>
2022-05-28 09:56:26 +0530

    sm6150-common: audio: Import dolby codecs
    Change-Id: Iccbc8553fd3e93215cf06bc4eed0e004228d8231
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -133,7 +133,8 @@ PRODUCT_COPY_FILES += \
 
 # Dolby
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
 
 TARGET_EXCLUDES_AUDIOFX := true
 
