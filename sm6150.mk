f9652bec64317dcfce31e87044c0e57608129088
Pranav Vashi <neobuddy89@gmail.com>
2022-06-23 02:22:09 +0530

    sm6150-common: Disable zram writeback
    * Disabled on many devices with reports to lagginess in long term and random reboot in some instances.
    
    Change-Id: I73985159766da08cd35b0288db29cfafc7db62b0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -384,9 +384,3 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
-
-# ZRAM writeback
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.zram.mark_idle_delay_mins=60 \
-    ro.zram.first_wb_delay_mins=1440 \
-    ro.zram.periodic_wb_delay_hours=24
