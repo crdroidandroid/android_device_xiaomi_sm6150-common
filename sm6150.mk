964625936c995ea315d1544f8e57f30405ee17f8
Igor121 <chernokalsky89@gmail.com>
2022-07-16 13:02:58 +0300

    sm6150-common: Enable blur * looks good

--- sm6150.mk
+++ sm6150.mk
@@ -94,6 +94,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
