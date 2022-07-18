0266b7511ba3ef475de4287d266e3e4f9e491e84
Golbinex <2061409-Golbinex@users.noreply.gitlab.com>
2022-07-18 19:29:46 +0200

    Revert "sm6150-common: Switch to dot product CPU variant"
    This reverts commit 9d1ec6de39a1064e3951dfaa542080ef670a0437.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,12 +23,12 @@ include vendor/xiaomi/sm6150-common/BoardConfigVendor.mk
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := cortex-a76
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a76
