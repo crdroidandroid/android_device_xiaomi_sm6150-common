2d1afaec8dbb036dcf4cb4e8baccfbefaba53a14
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-08-21 08:34:51 +0000

    sm6150-common: Define AAPT configuration
    Change-Id: Ic12fc243a6b86a0972ef5dc0bb26148be540be57
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -20,6 +20,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sm6150-common/sm6150-common-vendor.mk)
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
