e64d5ce3010f65ba783cfb4e9cf32759026e7879
Pranav Vashi <neobuddy89@gmail.com>
2022-09-03 18:03:59 +0530

    sm6150-common: Switch to AOSP Bluetooth
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -1,11 +1,10 @@
 /*
- *
  *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
  *  Not a Contribution, Apache license notifications and license are retained
  *  for attribution purposes only.
  *
  * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2018-2020 The LineageOS Project
+ * Copyright (C) 2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,64 +22,12 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-
-#include <cutils/properties.h>
-#include <string.h>
-
-#include "osi/include/osi.h"
-
-#pragma push_macro("PROPERTY_VALUE_MAX")
-
-typedef struct {
-    const char *product_device;
-    const char *product_region;
-    const char *product_sku;
-    const char *product_model;
-} device_t;
-
-static const device_t devices[] = {
-    {"davinci", "CN", "ALL", "Xiaomi Redmi K20"},
-    {"davinci", "GLOBAL", "ALL", "Xiaomi Mi 9T"},
-    {"davinciin", "INDIA", "ALL", "Xiaomi Redmi K20"},
-    {"phoenix", "ALL", "ALL", "Redmi K30"},
-    {"phoenixin", "INDIA", "ALL", "POCO X2"},
-    {"sweet", "ALL", "ALL", "Redmi Note 10 Pro"},
-    {"sweetin", "INDIA", "std", "Redmi Note 10 Pro"},
-    {"sweetin", "INDIA", "pro", "Redmi Note 10 Pro Max"},
-};
-
-static inline const char *BtmGetDefaultName()
-{
-    char product_device[PROPERTY_VALUE_MAX];
-    char product_region[PROPERTY_VALUE_MAX];
-    char product_sku[PROPERTY_VALUE_MAX];
-    property_get("ro.product.device", product_device, "");
-    property_get("ro.boot.hwc", product_region, "");
-    property_get("ro.boot.product.hardware.sku", product_sku, "");
-
-    for (unsigned int i = 0; i < ARRAY_SIZE(devices); i++) {
-        device_t device = devices[i];
-
-        if (strcmp(device.product_device, product_device) == 0 &&
-               (strcmp(device.product_region, product_region) == 0 ||
-               strcmp(device.product_region, "ALL") == 0) &&
-               (strcmp(device.product_sku, product_sku) == 0 ||
-               strcmp(device.product_sku, "ALL") == 0)) {
-           return device.product_model;
-       }
-    }
-
-    // Fallback to ro.product.model
-    return "";
-}
-
-#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    32
+#define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-#define GATT_MAX_PHY_CHANNEL  10
-
-#pragma pop_macro("PROPERTY_VALUE_MAX")
-
+// skips conn update at conn completion
+#define BT_CLEAN_TURN_ON_DISABLED 1
+/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
+#define AVDT_NUM_SEPS 12
 #endif
