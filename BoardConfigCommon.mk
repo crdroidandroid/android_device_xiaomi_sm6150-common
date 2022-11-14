e47a99e7f0ac390d0a1acaf870050b3e6bf55bd9
Michael Bestas <mkbestas@lineageos.org>
2022-11-14 13:11:09 +0200

    sm6150-common: Compile the kernel without GCC
    Change-Id: I882c0d8600f88c1bb939c7ee247230ec5e70338a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -81,6 +81,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
 TARGET_KERNEL_CLANG_VERSION := r563880c
+TARGET_KERNEL_NO_GCC := true
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
