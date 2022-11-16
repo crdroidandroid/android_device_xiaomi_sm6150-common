8b9ff53180589009fcc1ef7cbaf724d1685846ec
Anush02198 <anush.4376@gmail.com>
2022-11-16 15:08:39 -0500

    sm6150-common:Allign sepolicies as-per user build
    Signed-off-by: Anush02198 <anush.4376@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -227,7 +227,6 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
