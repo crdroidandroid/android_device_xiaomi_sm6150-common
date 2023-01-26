4fe990bca21d0b01b101eb6339bf44a07a6194e2
Pranav Vashi <neobuddy89@gmail.com>
2023-01-26 08:25:00 +0530

    sm6150-common: Add missing camera packages
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -133,9 +133,11 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl:64 \
     android.hardware.camera.provider@2.4-service_64 \
+    libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
     libpiex_shim \
+    libstdc++.vendor \
     libxml2 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
