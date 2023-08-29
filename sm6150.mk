f977c3bae83ab54fc7ad1f9c9ef061b3efa6dccb
basamaryan <basam.aryan@gmail.com>
2023-08-29 15:33:12 +0200

    sm6150-common: Update Adreno to Moto G84 (bangkk)
    Change-Id: Ia143fd2098db3c11ee9428c93ba7d83c626db2ae

--- sm6150.mk
+++ sm6150.mk
@@ -140,7 +140,8 @@ PRODUCT_PACKAGES += \
     disable_configstore
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
 
 # DRM
 PRODUCT_PACKAGES += \
