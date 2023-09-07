d58aec2f7a79ac576987fc10dfb53c44394e404f
Golbinex <2061409-Golbinex@users.noreply.gitlab.com>
2023-09-07 16:00:30 +0200

    Revert "sm6150-common: Welcome PixelOS"
    This reverts commit a40ad3deb53ee6fd812cfe34b8154bcf4c45f482.

--- sm6150.mk
+++ sm6150.mk
@@ -284,7 +284,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
