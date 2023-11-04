fa7170e6101fd41e25d2f927ab1cd52e6511481f
Pranav Vashi <neobuddy89@gmail.com>
2023-11-04 21:31:01 +0530

    sm6150-common: Switch to wifi service aidl
    Change-Id: I48d5fd962f3fb228f0ec8636e26cd1bfd15e14d6

--- sm6150.mk
+++ sm6150.mk
@@ -535,7 +535,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     ipacm \
     IPACM_cfg.xml \
