438f0fee939a18e7e636e6a7d85ce96b41ddaa00
Pranav Vashi <neobuddy89@gmail.com>
2023-11-27 15:47:40 +0530

    Revert "Temp Revert "sm6150-common: Build lineage health HAL""
    This reverts commit 48157b7529d83da12c52567e24a87780f74a013e.

--- sm6150.mk
+++ sm6150.mk
@@ -267,6 +267,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi_sm6150
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.xiaomi_sm6150
