00d0b8e938fc16895174bc4c29dd01a3745e5bdd
meloalfa159 <melolongo87@gmail.com>
2024-03-26 11:55:46 +0100

    sm6150: parts: Convert to SwitchPreferenceCompat

--- parts/src/org/lineageos/settings/speaker/ClearSpeakerFragment.java
+++ parts/src/org/lineageos/settings/speaker/ClearSpeakerFragment.java
@@ -27,7 +27,7 @@ import android.util.Log;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 
 import org.lineageos.settings.R;
 
@@ -42,7 +42,7 @@ public class ClearSpeakerFragment extends PreferenceFragment implements
 
     private Handler mHandler = new Handler(Looper.getMainLooper());
     private MediaPlayer mMediaPlayer;
-    private SwitchPreference mClearSpeakerPref;
+    private SwitchPreferenceCompat mClearSpeakerPref;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
