263bce50ea78c94282d6c1c32a2ea4a3c6ea1739
Pranav Vashi <neobuddy89@gmail.com>
2024-04-05 23:29:43 +0530

    sm6150-common: Set TARGET_PACKAGE_NAME for miui camera
    Change-Id: If6ad715b836d5650443002a41f789ebc5ec490f8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,6 +53,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Camera
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
+TARGET_CAMERA_PACKAGE_NAME := com.xiaomi.sessionparams.clientName
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/configs/config.fs
