ae93817911070cfe4a28442f40ce57dbd805a109
Pranav Vashi <neobuddy89@gmail.com>
2024-05-08 15:32:18 +0530

    sm6150-common: Include camera config when available
    Change-Id: Ica54cbb328ae2b11e2bf383225ef55e66d049f84
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -86,6 +86,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Camera
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
+
 $(call soong_config_set_bool,camera,override_format_from_reserved,true)
 
 PRODUCT_PACKAGES += \
