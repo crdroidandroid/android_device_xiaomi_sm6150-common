3fef095130a289eb9991690ee7d046db623aa5c7
Pranav Vashi <neobuddy89@gmail.com>
2024-05-08 15:32:18 +0530

    sm6150-common: Add common missing camera blobs
    Change-Id: Ica54cbb328ae2b11e2bf383225ef55e66d049f84

--- sm6150.mk
+++ sm6150.mk
@@ -88,6 +88,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Camera
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
+
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
