04924e23a04608d670f260e80545ba5680e9a12d
Pranav Vashi <neobuddy89@gmail.com>
2024-05-12 09:08:06 +0530

    sm6150-common: Use appropriate arch setup
    Change-Id: Ib687617676ee308b4336a9a8be76777821009f0a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,13 +23,15 @@ include vendor/xiaomi/sm6150-common/BoardConfigVendor.mk
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := cortex-a55
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a55
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
 
 # A/B
 AB_OTA_UPDATER := false
