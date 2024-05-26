e68c54a6796aa1b3f17516f538383a490cc00bf9
Pranav Vashi <neobuddy89@gmail.com>
2024-05-26 16:30:53 +0530

    sm6150-common: Enable blur
    Change-Id: If3e45ca2c0d798bd915bb5a34a732f71c752b22f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -91,6 +91,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # Camera
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
 
