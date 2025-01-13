02cae184c0be5a4a55ff35ef68d04da674d52da6
heScarastic <warabhishek@gmail.com>
2025-01-13 19:03:25 +0530

    sm6150-common: parts: Introduce Dirac from xiaomi sdm845-common LineageOS 1. sdm845-common: parts: Introduce Dirac preferences Add a frontend for Dirac sound optimization settings. Inject into sound preferences through settings IA.
    Change-Id: I37f6e64fa440c43dbe238cc65af5f9b0bbd60d83
    Signed-off-by: Joey <joey@lineageos.org>
    
    2. sdm845-common: parts: Add DiracSound backend
    Special thanks to vasishath for his base work on mi3
    
    Change-Id: Id81e60a18dc34c991b82a5a2685c9041222a6ea7
    
    3. sdm845-common: parts: Adapt to S style
     * Use Theme.SubSettingsBase for theme
     * Replace PreferenceActivity with CollapsingToolbarBaseActivity
     * Add Titles to preference screen
     * Remove onOptionsItemSelected and where necessary move to activity
    
    Change-Id: I87af15f5e165aa7bcd556eb2b716c0ce624ca4e7
    
    4. sdm845-common: parts: Move XiaomiParts to /system_ext
    Change-Id: Ic3ed16bb0bea98d9dd5da96da63c65ebe735036a
    
    5. sdm845-common: sepolicy: Add mlstrustedsubject attribute to parts
    This ensures that XiaomiParts has the proper permission to access
    /data/user/0/org.lineageos.settings/.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I5ed0e9677e6271d9fb003cac728d2e3bb10ddf4a

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
@@ -33,6 +34,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
+        DiracUtils.initialize();    
         DozeUtils.onBootCompleted(context);
         ThermalUtils.startService(context);
     }
