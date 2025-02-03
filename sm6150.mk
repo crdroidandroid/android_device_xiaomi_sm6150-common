617fe235d7f487ef3386d507ad3478928ced116d
Lafactorial <hknhzl@gmail.com>
2025-02-03 08:54:42 +0300

    sm6150-common: Drop live display

--- sm6150.mk
+++ sm6150.mk
@@ -225,10 +225,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay-service.xiaomi_sm6150
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
