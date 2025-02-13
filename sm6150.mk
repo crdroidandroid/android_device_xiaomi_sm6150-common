01f4aab1de0c6f3e233862fcf7386de5f39088f8
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-13 15:04:52 +0800

    sm6150-common: Switch to common QTI USB init scripts
    Change-Id: I42864edf1cf554815fa24b12892afd21835d13c7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -235,6 +235,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     init.qcom.sh \
+    init.qcom.usb.sh \
     init.qti.early_init.sh
 
 PRODUCT_PACKAGES += \
@@ -261,7 +262,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/qcom-caf/common/libqti-perfd-client \
-    hardware/xiaomi
+    hardware/xiaomi \
+    vendor/qcom/opensource/usb/etc
 
 # Telephony
 PRODUCT_PACKAGES += \
