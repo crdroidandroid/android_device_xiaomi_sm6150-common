a18e9f32de1b8f77d6b5064900fa017c99fab47f
Zeyy <zaidanagusta319@gmail.com>
2025-03-15 22:56:53 +0700

    sm6150-common: replace frameworks/base/config/boot-image-profile.txt to frameworks/base/boot/boot-image-profile.txt
    error: prebuilts/runtime/mainline/i18n/sdk/Android.bp:9:1: module prebuilt_i18n-bootclasspath-fragment variant android_common: source path frameworks/base/config/boot-image-profile.txt does not exist
    Signed-off-by: Zeyy <zaidanagusta319@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -413,7 +413,7 @@ PRODUCT_PACKAGES += \
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # System
 PRODUCT_PROPERTY_OVERRIDES += \
