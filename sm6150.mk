6439f6e548dc991f6e0d47489294d0b78b28b256
K4LCHAKRA <oseverest44@gmail.com>
2025-03-27 14:20:02 +0000

    sm6150-common: Fix VNDK version to resolve build failure
    The build was failing due to an incorrect or missing VNDK version (29).
    Setting PRODUCT_EXTRA_VNDK_VERSIONS := 30 resolved the issue,
    ensuring compatibility with the required vendor interface.
    
    Fixes: build/make/core/board_config.mk:1001: error: VNDK version 29 not found
    Signed-off-by: K4LCHAKRA <oseverest44@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -481,7 +481,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
-PRODUCT_EXTRA_VNDK_VERSIONS := 29 30
+PRODUCT_EXTRA_VNDK_VERSIONS := 30
 
 # Vulkan
 PRODUCT_PACKAGES += \
