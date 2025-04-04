117a4743494d1d521f5ebba45ea399e3d507eaf5
Pranav Vashi <neobuddy89@gmail.com>
2025-04-04 12:02:39 +0530

    sm6150-common: Build DeviceAsWebcam service
    * We disabled this by default on crdroid.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -93,6 +93,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Camera
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
 
 $(call soong_config_set_bool,camera,override_format_from_reserved,true)
