c2d80dbeefa204a8201f265b57c6611d81e8f040
Pranav Vashi <neobuddy89@gmail.com>
2025-11-18 21:58:44 +0530

    sm6150-common: Disable blur by default
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -90,9 +90,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
-# Blur
-TARGET_ENABLE_BLUR := true
-
 # Camera
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
 
