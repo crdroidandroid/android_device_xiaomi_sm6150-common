3cba2a63a78b109f9ee38457602c50d828132502
HyperN00B <bence.tari@gmail.com>
2026-01-31 12:41:18 +0100

    sm6150-common: Append Dolby manifests to BoardConfig

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,10 +53,15 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    hardware/dolby/dolby_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/hidl/manifest.xml
-DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
+DEVICE_MANIFEST_FILE += \
+    hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml \
+    hardware/dolby/vendor.dolby.hardware.dms@2.0-service.xml \
+    hardware/dolby/vendor.dolby.media.c2.xml
+
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 ODM_MANIFEST_FILES += $(COMMON_PATH)/configs/hidl/manifest-qva.xml
 
