fe56b063c12477bf27b8c098fe3f4ea0a78ceb5f
Pranav Vashi <neobuddy89@gmail.com>
2026-06-14 17:21:05 +0530

    Revert "sm6150-common: Compile kernel with clang 22.0.2"
    This reverts commit 63105f1aec8ed67ecbac14876b517379de8c29ed.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,7 +72,6 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
 TARGET_KERNEL_CONFIG := vendor/sdmsteppe-perf_defconfig
-TARGET_KERNEL_CLANG_VERSION := r596125
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1
