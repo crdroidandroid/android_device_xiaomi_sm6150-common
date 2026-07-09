ce7dd2e96a536f5003443efd8e2ab180e0d6632e
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-09 05:27:31 +0000

    sm6150-common: Use legacy libion implementation
    * At least one blob directly depends on this, with
      many other bits and pieces likely.
    
    Change-Id: Ida76ff1e910c04ba04e59d02ae8b91041b825d96
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -185,6 +185,9 @@ PRODUCT_PACKAGES += \
 # Init
 $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_sm6150)
 
+# ION
+$(call soong_config_set_bool,libion,legacy_impl,true)
+
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/security/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
